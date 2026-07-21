f903909ea233ec43e20fe3edce2ac109c6793828
ISHIKAWA Natsuki <napple02@gmail.com>
2026-07-21 15:35:17 +0900

    ボトムナビ、ガラス化

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  * 注意: アプリのロジック更新時は CACHE_VERSION を上げること。
  */
 
-const CACHE_VERSION = "v75";
+const CACHE_VERSION = "v76";
 const CACHE_NAME = `t-pod-${CACHE_VERSION}`;
 
 // 16進カラー（#rgb / #rgba / #rrggbb / #rrggbbaa）判定
